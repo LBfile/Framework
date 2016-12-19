e4aa953734035c6b86bfeb7b6d36c4e997315d84
lbfile <513206491@qq.com>
2016-12-19 15:10:22 +0800

    选择要提交的

---
+++ Console/src/com/console/ConsoleMain.java
@@ -0,0 +1,11 @@
+package com.console;
+
+import com.model.Book;
+
+public class ConsoleMain {
+	public static void main(String[] args) {
+		Book book =new Book();
+		book.setId("123456");
+		book.setName("book");
+	}
+}
