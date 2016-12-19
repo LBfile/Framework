9573db1c472e18020be763ee0d3bd72564c68c65
lbfile <513206491@qq.com>
2016-12-19 17:22:07 +0800

    制造冲突

--- Console/src/com/console/ConsoleMain.java
+++ Console/src/com/console/ConsoleMain.java
@@ -5,8 +5,9 @@ import com.model.Book;
 public class ConsoleMain {
 	public static void main(String[] args) {
 		Book book =new Book();
-		book.setId("1234567");
+		book.setId("003456700");
 		book.setName("book");
 		//紧急分支修复
+		System.out.println(book.getName());
 	}
 }
