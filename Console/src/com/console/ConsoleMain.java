d2057f7918f2cd20e874bf412077a2adde6cd2a8
lbfile <513206491@qq.com>
2016-12-22 15:03:07 +0800

    1

--- Console/src/com/console/ConsoleMain.java
+++ Console/src/com/console/ConsoleMain.java
@@ -5,7 +5,7 @@ import com.model.Book;
 public class ConsoleMain {
 	public static void main(String[] args) {
 		Book book =new Book();
-		book.setId("003456700");
+		book.setId("003456700 ");
 		book.setName("book");
 		//紧急分支修复
 		System.out.println(book.getName());
